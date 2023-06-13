41a214964a61636ba7e633c82207ed5f61446aa4
cdurhrt <dumingliang1995@163.com>
2023-06-13 14:47:51 +0800

    feat: updategp

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (mode === 'gp') {
+    return {
+      base:'/page-by-vite/'
+    }
+  }
+  if (command === 'serve') {
+    return {
+      // dev 独有配置
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build 独有配置
+    }
+  });
