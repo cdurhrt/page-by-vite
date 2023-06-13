fbbd70b380c99cf45b8cc87046406f5ae2a32f81
cdurhrt <dumingliang1995@163.com>
2023-06-13 21:21:50 +0800

    v

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,3 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  return {
-    base: "/page-by-vite/",
-  };
-  if (command === "serve") {
-    return {
-      // dev 独有配置
-    };
-  } else {
-    // command === 'build'
-    return {
-      // build 独有配置
-    };
-  }
-});
+export default defineConfig(({ command, mode, ssrBuild }) => {});
